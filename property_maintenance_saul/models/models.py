858ff56c539e673f7415bd253747d535c00a3f49
Saul <saul@pop-os.localdomain>
2022-10-31 10:42:54 -0600

    31OCT BACKUP

--- property_maintenance_saul/models/models.py
+++ property_maintenance_saul/models/models.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api
+import datetime
+
 
 class MaintenanceTeamModified(models.Model):
     _inherit = 'maintenance.team'
@@ -9,9 +11,10 @@ class MaintenanceTeamModified(models.Model):
 class AccountAssetModified(models.Model):
     _inherit = 'account.asset.asset'
 
-    maintenance_by_property = fields.One2many(
-        comodel_name='maintenance.request',
-        inverse_name='property_id')
+    maintenance_per_property = fields.One2many(
+        comodel_name='maintenance.property',
+        inverse_name='property_id',
+        store=True)
 
     def create_tenancy(self):
         return {
@@ -29,29 +32,40 @@ class AccountAssetModified(models.Model):
 
 
 class MaintenancePerProperty(models.Model):
-    _inherit = 'maintenance.request'
+    _name = 'maintenance.property'
 
-    #TODO: Volver a la tabla anterior con 1000 columnas
-
+    name = fields.Char(string='Nombre')
     team_id = fields.Many2one('maintenance.team', string="Equipo responsable")
     cost = fields.Float(string="Costo")
-    frequency = fields.Selection([('once', 'Unico'), ('Day', 'Diario'), ('Weekly', 'Semanal'), ('Monthly', 'Mensual'), ('Yearly', 'Anual')], default='once', string="Frecuencia")
-    charge_to_corrected = fields.Selection([('tenant','Inquilino'),('landlord','Propietario'),('admin','Administrador')], string="A cuenta de quien")
+    frequency = fields.Selection([('once', 'Unico'), ('Daily', 'Diario'), ('Weekly', 'Semanal'), ('Monthly', 'Mensual'), ('Yearly', 'Anual')], default='once', string="Frecuencia")
+    charge_to = fields.Selection([('tenant','Inquilino'),('landlord','Propietario'),('admin','Administrador')], string="A cuenta de quien")
     charge = fields.Boolean(string="Aplicar cargo")
+
+    property_id = fields.Many2one('account.asset.asset')
 
 
 class AccountAnalyticModified(models.Model):
     _inherit = 'account.analytic.account'
 
-    def _tentant(self):
-        # frequency = self.rent_type_id.renttype.lowercase()
-         
-        maintenances_return = []
-        maintenances = self.property_id.maintenance_by_property.search([("charge_to_corrected", "=", "tenant"), ("frequency", "=", "once")])
-        self.tenant_maintenance = maintenances
+    def _compute_maintenance(self):
+        related_recordset = self.property_id.maintenance_per_property.search(
+            [("charge_to", "=", "tenant"),
+             ("property_id", "=", self.property_id.id),
+             '|',
+             ("frequency", "=", self.frequency),
+             ("frequency", "=", "once")]
+        )
+        self.maintenance_per_property = related_recordset
 
-    tenant_maintenance = fields.One2many(
-        comodel_name='maintenance.request',
-        inverse_name='property_id',
-        compute="_tentant")
-    
+    def _compute_frequency(self):
+        for rec in self:
+            date_diff = (rec.date - rec.date)
+            if date_diff < datetime.timedelta(days=8):
+                rec.frequency = 'Daily'
+            elif date_diff < datetime.timedelta(days=32):
+                rec.frequency = 'Monthly'
+            else:
+                rec.frequency = 'Yearly'
+
+    maintenance_per_property = fields.One2many('maintenance.property', compute="_compute_maintenance")
+    frequency = fields.Selection([('Daily', 'Diario'), ('Weekly', 'Semanal'), ('Monthly', 'Mensual'), ('Yearly', 'Anual')], compute='_compute_frequency', string="Frecuencia")
--- property_maintenance_saul/property_maintenance_saul/models/models.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class MaintenanceTeamModified(models.Model):
-    _inherit = 'maintenance.team'
-
-    partner_id = fields.Many2one('res.partner', string='Company')
