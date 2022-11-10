aac018b90f47fbac2fb7a4ab54f8d852a6b3b543
ChaboCode <kaerdos.dev@gmail.com>
2022-11-10 09:13:42 -0600

    .

---
+++ custom_property_new/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': 'Custom Property',
+    'version': '13.0.1.0.0',
+    'category': 'Wb',
+    'summary': """
+            custom property:
+            
+     """,
+    'author': '',
+    'license': 'LGPL-3',
+    'website': '',
+    'depends': [
+       'property_management',
+       'property_landlord_management',
+       'om_account_asset',
+       'calendar',
+    ],
+    'data': [
+       'security/ir.model.access.csv',
+       'views/view_property_for_user.xml',
+       'views/count_pago_view.xml',
+       'views/Rent_type_view.xml',
+       'views/calendary_property.xml',
+       'report/report_contrator_email.xml',
+       'report/menu_report_contraro.xml',
+       'report/report_invoice_pago.xml',
+       'data/email_template.xml',
+    ],
+   
+}
--- property_maintenance_saul/models/models.py
+++ property_maintenance_saul/models/models.py
@@ -13,7 +13,7 @@ class AccountAssetModified(models.Model):
     _inherit = 'account.asset.asset'
 
     maintenance_per_property = fields.One2many(
-            comodel_name='maintenance.property',
+            comodel_name='maintenance.request',
             inverse_name='property_id',
             store=True)
 
@@ -31,7 +31,7 @@ class AccountAssetModified(models.Model):
                 'target': 'current',
                 'context': {
                     'default_property_id': self.id,
-                    'default_property_owner_id': self.property_owner,
+                    'default_property_owner_id': self.property_owner.id,
                     }
                 }
 
@@ -47,18 +47,18 @@ class MaintenanceNames(models.Model):
     _name = 'maintenance.names'
 
     name = fields.Char(string='Nombre de mantenimiento')
-    maintenances = fields.One2many('maintenance.property', inverse_name='name')
+    maintenances = fields.One2many('maintenance.request', inverse_name='name')
 
 
 
 class MaintenancePerProperty(models.Model):
-    _name = 'maintenance.property'
+    _inherit = 'maintenance.request'
 
     name = fields.Many2one('maintenance.names')
     team_id = fields.Many2one('maintenance.team', string="Equipo responsable")
     cost = fields.Float(string="Costo")
     frequency = fields.Selection([('once', 'Unico'), ('Daily', 'Diario'), ('Weekly', 'Semanal'), ('Monthly', 'Mensual'), ('semestre', 'Semestral'), ('Yearly', 'Anual')], default='once', string="Frecuencia")
-    charge_to = fields.Selection([('tenant','Inquilino'),('landlord','Propietario'),('admin','Administrador')], string="A cuenta de quien")
+    to_charge = fields.Selection([('tenant','Inquilino'),('landlord','Propietario'),('admin','Administrador')], string="A cuenta de quien")
     charge = fields.Boolean(string="Aplicar cargo")
 
     property_id = fields.Many2one('account.asset.asset')
@@ -66,49 +66,59 @@ class MaintenancePerProperty(models.Model):
     def name_get(self):
         context = self.env.context
         res = []
-        if context.get('special_field', False):
-            for rec in self:
-                res.append((rec.id, rec.name))
-        elif context.get('special_field', 'team'):
-            for rec in self:
-                res.append((rec.id, rec.team_id))
-        elif context.get('special_field', 'frequency'):
-            for rec in self:
-                res.append((rec.id, rec.frequency))
-            for rec in self:
-                res.append((rec.id, rec.cost))
-        elif context.get('special_field', 'cost'):
-            for rec in self:
-                res.append((rec.id, rec.cost))
+        translation = {'once': 'Unico', 'Daily': 'Diario', 'Weekly': 'Semanal', 'Monthly': 'Mensual', 'semestre': 'Semestral', 'Yearly': 'Anual'}
+        for rec in self:
+            res.append((rec.id, translation[rec.frequency]))
         return res
 
 
 
 class MaintenanceContract(models.Model):
-     _name = 'maintenance.contract'
+    _name = 'maintenance.contract'
 
-     maintenance_id = fields.Many2one('maintenance.property')
-     charge = fields.Boolean(string="Aplicar cargo")
-     analytic_id = fields.Many2one('account.analytic.account')
+    def _compute_name(self):
+        for rec in self:
+            rec.table_name = rec.maintenance_request.name.name
+
+    def _compute_team(self):
+        for rec in self:
+            rec.table_team = rec.maintenance_request.team_id
+
+    def _compute_cost(self):
+        for rec in self:
+            rec.table_cost = rec.maintenance_request.cost
+
+    def _compute_frequency(self):
+        for rec in self:
+            val = rec.maintenance_request.frequency
+            if val == 'once':
+                rec.table_frequency = 'Unico'
+            elif val == 'Daily':
+                rec.table_frequency = 'Diario'
+            elif val == 'Weekly':
+                rec.table_frequency = 'Semanal'
+            elif val == 'Monthly':
+                rec.table_frequency = 'Mensual'
+            elif val == 'semestre':
+                rec.table_frequency = 'Semestral'
+            elif val == 'Yearly':
+                rec.table_frequency = 'Anual'
+            else:
+                rec.table_frequency = 'Invalido'
+
+
+    maintenance_request = fields.Many2one('maintenance.request')
+    charge = fields.Boolean(string="Aplicar cargo")
+    analytic_id = fields.Many2one('account.analytic.account')
+    property_id = fields.Many2one('account.asset.asset')
+    table_name = fields.Char(compute='_compute_name', string='Nombre')
+    table_cost = fields.Float(compute='_compute_cost', string='Costo')
+    table_team = fields.Many2one('maintenance.team', compute='_compute_team', string='Equipo')
 
 
 
 class AccountAnalyticModified(models.Model):
     _inherit = 'account.analytic.account'
-
-    def _compute_maintenance(self):
-        if self.is_landlord_rent:
-            charge_to_query = 'landlord'
-        else:
-            charge_to_query = 'tenant'
-        related_recordset = self.property_id.maintenance_per_property.search(
-                [("charge_to", "=", charge_to_query),
-                    ("property_id", "=", self.property_id.id),
-                    '|',
-                    ("frequency", "=", self.frequency),
-                    ("frequency", "=", "once")]
-                )
-        self.maintenance_per_property = related_recordset
 
     def _compute_frequency(self):
         for rec in self:
@@ -124,7 +134,7 @@ class AccountAnalyticModified(models.Model):
             else:
                 rec.frequency = 'Yearly'
 
-    maintenance_per_property = fields.One2many('maintenance.property')
+    maintenance_per_property = fields.One2many('maintenance.contract', inverse_name='analytic_id')
     frequency = fields.Selection([('Daily', 'Diario'), ('Weekly', 'Semanal'), ('Monthly', 'Mensual'), ('Semestral', 'Semestral'), ('Yearly', 'Anual')], compute='_compute_frequency', string="Frecuencia")
     # Mirror contract: For tenant view
     mirror_contract_id = fields.Many2one('account.analytic.account')
@@ -132,23 +142,6 @@ class AccountAnalyticModified(models.Model):
     tenant_tenancy_id = fields.Many2one('account.analytic.account')
 
     def mirror_contract(self):
-        if not self.mirror_contract_id:
-            new_mirror = {
-                    'name': self.name,
-                    'property_id': self.property_id.id,
-                    'property_owner_id': self.property_id.property_owner.id,
-                    'date_start': self.date_start,
-                    'date': self.date,
-                    'chech_in': self.chech_in,
-                    'chech_out': self.chech_out,
-                    'ten_date': self.ten_date,
-                    'frequency': self.frequency,
-                    'is_landlord_rent': True,
-                    'tenant_tenancy_id': self.id,
-                    }
-            mirror_record = self.mirror_contract_id.create([new_mirror,])
-            self.mirror_contract_id = mirror_record[0].id
-
 
         return {
                 'type': 'ir.actions.act_window',
@@ -159,3 +152,95 @@ class AccountAnalyticModified(models.Model):
                 'res_id': self.mirror_contract_id.id,
                 'target': 'current',
                 }
+
+    def calular_precios_renta(self):  # FIXME: Typo
+        res = super(AccountAnalyticModified,self).calular_precios_renta()
+
+        for rec in self:
+            if rec.is_landlord_rent:
+                to_charge_query = 'landlord'
+            else:
+                to_charge_query = 'tenant'
+            related_recordset = rec.property_id.maintenance_per_property.search(
+                    [
+                        ("to_charge", "=", to_charge_query),
+                        ("property_id", "=", rec.property_id.id),
+                        '|',
+                        ("frequency", "=", rec.frequency),
+                        ("frequency", "=", "once")
+                        ]
+                    )
+            for maintenance in related_recordset:
+                if maintenance.frequency == 'Daily':
+                    times = 0
+                    while times < rec.date - rec.date_start:
+                        self.add_maintenance(rec, rec.date_start + times, maintenance)
+                        times += 1
+                elif maintenance.frequency == 'Weekly':
+                    times = 0
+                    while times < rec.date - rec.date_start:
+                        self.add_maintenance(rec, rec.date_start + times*8, maintenance)
+                        times += 1
+                elif maintenance.frequency == 'Montly':
+                    times = 0
+                    while times < rec.date - rec.date_start:
+                        self.add_maintenance(rec, rec.date_start + times*32, maintenance)
+                        times += 1
+                elif maintenance.frequency == 'Semestral':
+                    times = 0
+                    while times < rec.date - rec.date_start:
+                        self.add_maintenance(rec, rec.date_start + times*30*6, maintenance)
+                        times += 1
+                elif maintenance.frequency == 'Yearly':
+                    times = 0
+                    while times < rec.date - rec.date_start:
+                        self.add_maintenance(rec, rec.date_start + times*365, maintenance)
+                        times += 1
+                else:
+                    self.add_maintenance(rec, rec.date, maintenance)
+
+            maintenance_entries = self.env['maintenance.contract'].search([('analytic_id', '=', rec.id)])
+            rec.maintenance_per_property = maintenance_entries
+
+        # Mirror contract
+        for rec in self:
+            if not rec.mirror_contract_id:
+                new_mirror = {
+                        'name': rec.name,
+                        'property_id': rec.property_id.id,
+                        'property_owner_id': rec.property_id.property_owner.id,
+                        'date_start': rec.date_start,
+                        'date': rec.date,
+                        'chech_in': rec.chech_in,
+                        'chech_out': rec.chech_out,
+                        'ten_date': rec.ten_date,
+                        'frequency': rec.frequency,
+                        'is_landlord_rent': True,
+                        'tenant_tenancy_id': rec.id,
+                        }
+                mirror_record = rec.mirror_contract_id.create([new_mirror,])
+                rec.mirror_contract_id = mirror_record[0].id
+
+
+        return res
+
+    def add_maintenance(self, rec, date, maintenance):
+        new_maintenance = {
+                'maintenance_request': maintenance.id,
+                'analytic_id': rec.id,
+                'property_id': rec.property_id.id,
+                'charge': True, 
+                }
+
+        maintenance_entry = rec.maintenance_per_property.create([new_maintenance,])
+        maintenance_entry.maintenance_request.schedule_date = date
+
+    def action_invoice_payment(self):
+        res = super(AccountAssetModified, self).action_invoice_payment() 
+
+        for rec in self:
+            for maintenance in rec.maintenance_per_property:
+                pass
+
+
+
